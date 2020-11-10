2a61b99a0631cdf488ffcefc037d213e191be3ab
zhangyi <yi.zhangG007453@eslink.com>
2020-11-10 11:12:49 +0800

    feat:增加

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: zhangyi
+ * @Date: 2020-11-06 16:37:46
+ * @LastEditors: zhangyi
+ * @LastEditTime: 2020-11-10 09:44:17
+-->
+<script>
+import { value, onMounted, onUnmounted } from 'vue';
+function useMouse() {
+  let x = value(0);
+  const changeValue = () => {
+    x.value ++;
+  }
+  onMounted(() => {
+    window.addEventListener('click', changeValue)
+  })
+  onUnmounted(() => {
+    window.removeEventListener('click', changeValue)
+  })
+  return x;
+}
+
+export default useMouse;
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: zhangyi
+ * @Date: 2020-11-06 16:28:51
+ * @LastEditors: zhangyi
+ * @LastEditTime: 2020-11-10 09:59:30
+-->
+<template>
+	<div class="hello">
+		<button @click="addCount">{{count}}</button>
+	</div>
+</template>
+
+<script>
+import { ref,watchEffect,onMounted } from 'vue';
+export default {
+
+    setup() {
+		let count = ref(0); 
+        const addCount = () => {
+            count.value++;
+		};
+		watchEffect(() => console.log(count.value))
+		onMounted(() => console.log('mounted!'));
+		
+		return { count, addCount};
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
